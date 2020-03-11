7654bdb1a79eab768fd9c1a6469742b7a62d1aef
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2020-03-11 15:32:56 -0700

    added Trie Data Structure - includes Insert, Search, Delete methods

---
+++ Trie/_Trie/trie.js
@@ -0,0 +1,103 @@
+const { trieNode } = require('./trieNode');
+/**
+ * define trie data structure 
+ * 
+ * methods: 
+ * - insert
+ * - search
+ * - delete
+ */
+
+
+const root_value = '';
+
+const trie = class Trie { 
+  constructor() {
+    // root node contains root_value and false 
+    this.root = new trieNode(root_value);
+  }
+
+  /** 
+   * @param { String } word - word that should be inserted into the Trie
+   */
+
+  insert(word) {
+    let current_Node = this.root;
+
+    for (let i = 0; i < word.length; i++) {
+      let isCompletedWord = false;
+
+      // set temp variable isCompletedWord to true when we reach the last letter of the string 
+      if (i === word.length - 1) {
+        isCompletedWord = true;
+      }
+
+      current_Node = current_Node.insert(word[i], isCompletedWord)
+    }
+
+    return this;
+  }
+
+  // search method
+  search(word) {
+    let current_Node = this.root;
+
+    for (let i = 0; i < word.length; i++) {
+      // if node has no children return an indicator such as null, else set current_node to the found node 
+      // if (Object.values(current_Node).length < 1 || !current_Node.children[word[i]]) {
+      if (!current_Node.children[word[i]]) {
+        return null;
+      }
+
+      // set current_Node to current_Node
+      current_Node = current_Node.children[word[i]];
+    }
+
+    return current_Node;
+  }
+
+  // delete method
+  delete(word) {
+    // call the search method to get the end of the node
+    // check for children - if node has no children, delete it - else if it has children, change the isCompleted to false to signify that it is no longer the end of a complere word 
+    // keep repeating the process until we find children
+    
+    // inner recursive function 
+    const deleteFromBottomToTop = (currentNode, index = 0) => {
+      // return if character index counter is equal or exceeding word.length - 1;
+      if (index >= word.length) {
+        //terminate function 
+        return;
+      }
+
+      const character = word[index];
+      const nextNode = currentNode.children[character];
+
+      // define base case if the word doesn't exist 
+      if (!nextNode) {
+        // if children doesn't contain the character we're looking for, terminate and return 
+        return;
+      }
+
+      // recurse to the next level Params: child node, increment index by 1
+      deleteFromBottomToTop(nextNode, index + 1);
+
+      // set value of last character's node to false to state that it is no longer a completed word
+      if (index == word.length - 1) {
+        nextNode.isCompleted = false;
+      }
+
+      // call trieNode delete method 
+      currentNode.delete(character);
+    };
+
+    // invoke recursive function and input the root node 
+    deleteFromBottomToTop(this.root);
+
+    return this;
+  }
+};
+
+module.exports = { 
+  trie,
+};
---
+++ Trie/_Trie/trieNode.js
@@ -0,0 +1,65 @@
+/**
+ * define individual trie nodes
+ * 
+ * methods: 
+ * - insert
+ * - delete 
+ */ 
+
+/** 
+ * @class 
+ * @param {Char} character 
+ * @param {Boolean} isCompleted - if the node is the representation of a completed word 
+ */
+
+const trieNode = class TrieNode { 
+  constructor(character, isCompleted = false) {
+    this.character = character;
+    this.isCompleted = isCompleted;
+    this.children = {}; // is it better to use an array or is it better to use to object? object here is to create an implementation of a HashMap 
+  }
+
+  isEmpty(children) {
+    // only return a truthy value if the length is greater than 0
+    return Object.values(children).length !== 0;
+  }
+
+  // insert method
+  insert(character, isCompleted = false) {
+    //check if child has char
+    if (!this.children[character]) {
+
+    // if char doesn't exist, instantiate new node with the character as the key for reference 
+      this.children[character] = new trieNode(character, isCompleted);
+    }
+
+    // if char does exist, set a variable to that node, check to see if that char represents end of the word
+    let childNode = this.children[character];
+
+    // if it is, change isComplete to true else, use false
+    childNode.isCompleted = childNode.isCompleted || isCompleted;
+
+    // return childNode whether it was existing or just created
+    return childNode;
+
+  }
+
+  // delete method
+  delete(character) {
+    // set current node to a variable 
+    let childNode = this.children[character];
+
+    // check node exists, if there are any children, if it is the end of a word 
+    if (childNode && !this.isEmpty(childNode.children) && !childNode.isCompleted) {
+      // delete childNode that is being targeted
+      delete this.children[character];
+    }
+    
+    return this;
+  }
+
+};
+
+module.exports = {
+   trieNode, 
+};
