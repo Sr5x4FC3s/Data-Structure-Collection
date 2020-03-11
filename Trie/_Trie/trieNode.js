7e66ac0abdbc0f0385ed04912b0ef6d80b7ee950
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2020-03-11 15:46:28 -0700

    added method definitions before each method

--- Trie/_Trie/trieNode.js
+++ Trie/_Trie/trieNode.js
@@ -8,6 +8,7 @@
 
 /** 
  * @class 
+ * @constructor
  * @param {Char} character 
  * @param {Boolean} isCompleted - if the node is the representation of a completed word 
  */
@@ -19,12 +20,24 @@ const trieNode = class TrieNode {
     this.children = {}; // is it better to use an array or is it better to use to object? object here is to create an implementation of a HashMap 
   }
 
+
+  /**
+   * @param { TrieNode Object } children - object with the children of a parent node 
+   * @return { Boolean } - returns whether or not an object has children or is empty
+   */
+
   isEmpty(children) {
     // only return a truthy value if the length is greater than 0
     return Object.values(children).length !== 0;
   }
 
-  // insert method
+
+  /** 
+   * @param { Char } character - character to be inserted 
+   * @param { Boolean } isCompleted - boolean value that determines whether or not the letter and the Trie node will represent the end of a complete word  
+   * @return { TrieNode }
+   */
+
   insert(character, isCompleted = false) {
     //check if child has char
     if (!this.children[character]) {
@@ -44,7 +57,12 @@ const trieNode = class TrieNode {
 
   }
 
-  // delete method
+
+  /** 
+   * @param { Char } character 
+   * @return { TrieNode } 
+   */
+
   delete(character) {
     // set current node to a variable 
     let childNode = this.children[character];
